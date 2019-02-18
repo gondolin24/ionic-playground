115b2b01c89e6d8d35d030094f8c02764bf54bc0
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2019-02-17 23:57:45 -0500

    added save functionality

--- ionic-playground/src/app/home/home.page.ts
+++ ionic-playground/src/app/home/home.page.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Storage} from '@ionic/storage';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+    constructor(private storage: Storage) {
+        this.storage.get('counter').then((val) => {
+            if (val) {
+                this.count = val;
+            }
+
+        });
+    }
+
+    private count = 0;
+
+    getCounter(): string {
+        return '' + this.count;
+    }
+
+    incrementScore() {
+        this.count++;
+        this.storage.set('counter', this.count);
+    }
+
 
 }
---
+++ ionic-playground/src/app/numberCruncher/number-util.ts
@@ -0,0 +1,8 @@
+export class NumberUtility {
+
+
+    public static getRandomNumber(range: number) {
+        return Math.floor(Math.random() * range);
+    }
+
+}
